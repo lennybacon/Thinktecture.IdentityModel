043afd88d4f65c1b85590c4a384737c867a14766
Daniel Fisher <info@lennybacon.com>
2014-12-31 13:10:59 +0100

    Update NonceGenerator.cs
    Better performance, less memory allocation

--- source/Hawk/Core/Helpers/NonceGenerator.cs
+++ source/Hawk/Core/Helpers/NonceGenerator.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-
+using System.Security.Cryptography;
 namespace Thinktecture.IdentityModel.Hawk.Core.Helpers
 {
     /// <summary>
@@ -9,34 +9,29 @@ namespace Thinktecture.IdentityModel.Hawk.Core.Helpers
     /// </summary>
     public class NonceGenerator
     {
-        private static int seed = Environment.TickCount;
-
-        private static ThreadLocal<Random> generator = new ThreadLocal<Random>(() =>
-            new Random(Interlocked.Increment(ref seed))
-        );
-
-        private static Random Generator
-        {
-            get
-            {
-                return generator.Value;
-            }
-        }
-
+        private static RNGCryptoServiceProvider rngProvider = new RNGCryptoServiceProvider();
         /// <summary>
         /// Generates a nonce matching the specified length and returns the same. Default length is 6.
         /// </summary>
         public static string Generate(int length = 6)
         {
-            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-            int min = 0;
-            int max = alphabet.Length - 1;
-
-            char[] randomCharacters = Enumerable.Range(0, length)
-                                            .Select(i => alphabet[Generator.Next(min, max)])
-                                                .ToArray();
-            return new String(randomCharacters);
+            var random = new List<char>();
+            do
+            {
+                byte[] bytes = new byte[length * 8];
+                rngProvider.GetBytes(bytes);
+                var characters = 
+                  bytes.Where(
+                      b => 
+                        (b >= 48 && b <= 57) || // 0 - 9
+                        (b >= 97 && b <= 122) || // a - z
+                        (b >= 65 && b <= 90)) // A - Z
+                    .Take(length - random.Count)
+                    .Select(b => Convert.ToChar(b));
+                random.AddRange(characters);
+            }
+            while (random.Count < length);
+            return new string(random.ToArray());
         }
     }
 }
