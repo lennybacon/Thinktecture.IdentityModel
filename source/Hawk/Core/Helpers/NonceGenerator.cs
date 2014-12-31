1e840d9e090dc02ddb8ef11cb85ad13f9bacef03
Daniel Fisher <info@lennybacon.com>
2014-12-31 13:06:44 +0100

    Update NonceGenerator.cs
    Better performance and less resource allocation

--- source/Hawk/Core/Helpers/NonceGenerator.cs
+++ source/Hawk/Core/Helpers/NonceGenerator.cs
@@ -10,6 +10,7 @@ namespace Thinktecture.IdentityModel.Hawk.Core.Helpers
     /// </summary>
     public class NonceGenerator
     {
+        private static RNGCryptoServiceProvider rngProvider = new RNGCryptoServiceProvider();
         /// <summary>
         /// Generates a nonce matching the specified length and returns the same. Default length is 6.
         /// </summary>
@@ -20,11 +21,8 @@ namespace Thinktecture.IdentityModel.Hawk.Core.Helpers
             do
             {
                 byte[] bytes = new byte[length * 8];
-                using (var rngProvider = new RNGCryptoServiceProvider())
-                {
-                    rngProvider.GetBytes(bytes);
-                }
-
+                rngProvider.GetBytes(bytes);
+                
                 var characters = bytes.Where(b => (b >= 48 && b <= 57) ||       // 0 - 9
                                                     (b >= 97 && b <= 122) ||    // a - z
                                                     (b >= 65 && b <= 90))       // A - Z
